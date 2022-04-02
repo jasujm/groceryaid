672ba0d806bc67574be7ff5108abbc226645fa52
Jaakko Moisio <jaakko@moisio.fi>
2022-04-02 13:39:33 +0300

    Add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+import "./App.scss";
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <h1>Grocery Aid</h1>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
